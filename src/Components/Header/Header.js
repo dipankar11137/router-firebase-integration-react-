eff768506075215813434b833f21033e52677738
Dipankar <dipankar15-11137@diu.edu.bd>
2022-04-09 16:33:32 +0600

    added on state change

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../Hooks/useFirebase';
 import './Header.css'
 
 const Header = () => {
+    const { user, handelSignOut } = useFirebase();
     return (
         <div className='header'>
             <nav>
                 <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/reviews">Reviews</Link>
+                <Link to="/register">Register</Link>
+                <span>{user?.displayName && user.displayName}</span>
+                {
+                    user?.uid
+                        ?
+                        <button onClick={handelSignOut}>Sign Out</button>
+                        :
+                        <Link to="/login">Login</Link>
+                }
             </nav>
         </div>
     );
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useFirebase from '../Hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
-            <h3>Please Login</h3>
+            <h3 className='fw-bold mt-3'>Please Login</h3>
+            <div className='p-3'>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+
+            <form >
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <br />
+                <input className='mt-2' type="password" name="" id="" placeholder='Password' />
+                <br />
+                <input className='mt-2' type="submit" value="Login" />
+            </form>
         </div>
     );
 };
